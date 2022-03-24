f4f5ea6c4e2e6572c2907e5892181e6ead007e2d
Boris Scharinger <41014115+Boris-Scharinger@users.noreply.github.com>
2022-03-24 15:44:34 +0100

    update to mermaid 8.14

---
+++ typings/MermaidForMendixProps.d.ts
@@ -0,0 +1,33 @@
+/**
+ * This file was generated from MermaidForMendix.xml
+ * WARNING: All changes made to this file will be overwritten
+ * @author Mendix UI Content Team
+ */
+import { CSSProperties } from "react";
+import { ActionValue, DynamicValue, EditableValue } from "mendix";
+
+export type BootstrapStyleEnum = "default" | "primary" | "success" | "info" | "inverse" | "warning" | "danger";
+
+export interface MermaidForMendixContainerProps {
+    name: string;
+    class: string;
+    style?: CSSProperties;
+    tabIndex?: number;
+    valueAttribute?: EditableValue<string>;
+    themeCSS: string;
+    showSVGdownloadButton: boolean;
+    downloadButtonLabelText: DynamicValue<string>;
+    bootstrapStyle: BootstrapStyleEnum;
+    onClickAction?: ActionValue;
+}
+
+export interface MermaidForMendixPreviewProps {
+    class: string;
+    style: string;
+    valueAttribute: string;
+    themeCSS: string;
+    showSVGdownloadButton: boolean;
+    downloadButtonLabelText: string;
+    bootstrapStyle: BootstrapStyleEnum;
+    onClickAction: {} | null;
+}
